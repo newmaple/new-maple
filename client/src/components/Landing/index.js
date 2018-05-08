8351cedab89e1a6b79b43630c7c03ee4dbbc6385
Daniel Tabak <daniel@serindlabs.com>
2018-05-08 16:17:57 -0400

    Fix merge conflict

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -3,7 +3,6 @@ import Header from './components/Header';
 
 require('./index.css');
 
-<<<<<<< HEAD
 class Landing extends React.Component {
     render() {
         return (
@@ -13,12 +12,5 @@ class Landing extends React.Component {
         );
     }
 }
-=======
-const Landing = () => (
-    <div>
-        Landing
-    </div>
-)
->>>>>>> e94e60a8b6bd5599e726e043f331597efa9f581b
 
 export default Landing;
