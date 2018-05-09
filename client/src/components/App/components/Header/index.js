28e66fd611ce9bbce7d978177e80622ea85bb6c9
Daniel Tabak <daniel@serindlabs.com>
2018-05-09 12:36:23 -0400

    Move Header to App container

--- client/src/components/App/components/Header/index.js
+++ client/src/components/App/components/Header/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import styles from "./index.css";
 import cn from 'classnames';
+import {Link} from "react-router-dom";
+
+import styles from "./index.css";
 
 const Header = () => (
   <div className={styles.Header}>
     <div className={cn("container", styles.Container)}>
-      <img style={{height: '89px'}} src={require("./logo.png")} alt="New Maple Holdings" />
+      <Link to="/">
+        <img style={{height: '89px'}} src={require("./logo.png")} alt="New Maple Holdings" />
+      </Link>
       <div className={styles.Nav}>
         <a className={styles.NavItem}>COMPANY</a>
         <a className={styles.NavItem}>PARTNER WITH US</a>
-        <a className={styles.NavItem}>CONTACT US</a>
+        <Link to="/contact-us" className={styles.NavItem}>CONTACT US</Link>
         <a className={styles.NavItemButton}>LOGIN</a>
       </div>
     </div>
