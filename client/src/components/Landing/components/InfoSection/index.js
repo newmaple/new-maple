f53f4ba396cdb821dc466cc3ad621f3f67128e9b
Daniel Tabak <daniel@serindlabs.com>
2018-05-11 17:57:15 -0400

    Add background video

--- client/src/components/Landing/components/InfoSection/index.js
+++ client/src/components/Landing/components/InfoSection/index.js
@@ -1,30 +1,66 @@
-import React from 'react';
-import styles from './index.css';
+import React from "react";
+import styles from "./index.css";
 import DownArrow from "./down_arrow.svg";
 
-const infoBgSrc = require('src/assets/img/infoBg.jpg');
+const scrollIntoView = selector => {
+  document.querySelector(selector).scrollIntoView({
+    behavior: "smooth"
+  });
+};
 
-const scrollIntoView = (selector) => {
-    document.querySelector(selector).scrollIntoView({ 
-        behavior: 'smooth' 
-    });
-}
+// const defaultOptions = {
+//     loop: true,
+//     autoplay: true,
+//     animationData: {
+//         ...animationData,
+//         assets: animationData.assets.map(
+//             asset => {
+//                 if (asset.u === "images/") {
+//                     return {
+//                         ...asset,
+//                         u: '',
+//                         p: require("./" + asset.u + asset.p)
+//                     }
+//                 } else {
+//                     return asset;
+//                 }
+//             }
+//         )
+//     }
+//   };
 
 const InfoSection = () => (
-    <div className={styles.InfoSection} style={{backgroundImage:`url(${infoBgSrc})`}}>
-        <div className={styles.InfoSectionContainer + " container"}>
-        <h2>A new breed.</h2>
-        <div className={styles.Description}>Premium Canadian Cannabis by connisseurs, for connisseurs.</div>
-        <div className={styles.NavigationSection}>
-            <ul>
-                <li onClick={() => scrollIntoView('#AboutSection')}>About us<DownArrow /></li>
-                <li onClick={() => scrollIntoView('#GrowersSection')}>Growers<DownArrow /></li>
-                <li onClick={() => scrollIntoView('#TeamSection')}>Our team<DownArrow /></li>
-                <li onClick={() => scrollIntoView('#InvestorSection')}>Investment<DownArrow /></li>
-            </ul>
-        </div>
-        </div>
+  <div className={styles.InfoSection}>
+
+
+    <div className={styles.InfoSectionContainer + " container"}>
+      <h2>A new breed.</h2>
+      <div className={styles.Description}>
+        Premium Canadian Cannabis by connisseurs, for connisseurs.
+      </div>
+      <div className={styles.NavigationSection}>
+        <ul>
+          <li onClick={() => scrollIntoView("#AboutSection")}>
+            About us<DownArrow />
+          </li>
+          <li onClick={() => scrollIntoView("#GrowersSection")}>
+            Growers<DownArrow />
+          </li>
+          <li onClick={() => scrollIntoView("#TeamSection")}>
+            Our team<DownArrow />
+          </li>
+          <li onClick={() => scrollIntoView("#InvestorSection")}>
+            Investment<DownArrow />
+          </li>
+        </ul>
+      </div>
     </div>
+    <video
+      className={styles.InfoSectionBackground}
+      autoPlay
+      src={require("./background.mp4")}
+    />
+  </div>
 );
 
 export default InfoSection;
